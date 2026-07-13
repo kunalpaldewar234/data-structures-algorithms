03d06d1ffabe83cde4f18b3c2486dc0ea83f67f2
kunal paldewar <kunalpaldewar06@gmail.com>
2026-07-13 19:28:35 +0530

    Time: 0 ms (100%), Space: 14.5 MB (17%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+       stack<char>st;
+       string res;
+       for(int i=0;i<s.length();i++){
+        if(st.empty()){
+            st.push(s[i]);
+            continue;
+        }
+        if(st.top() == s[i]){
+            st.pop();
+            continue;
+        }
+        st.push(s[i]);
+       } 
+
+       while(!st.empty()){
+        res.push_back(st.top());
+        st.pop();
+       }
+       reverse(res.begin(),res.end());
+       return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
