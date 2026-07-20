8d826ce30aedca9987399a97dc6c35613dbfb569
kunal paldewar <kunalpaldewar06@gmail.com>
2026-07-20 23:44:00 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.66%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low<=high){
+            int mid = (high+low)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
