022023817a62cd69f59bb426b2e9fafba10a8dd7
kunal paldewar <kunalpaldewar06@gmail.com>
2026-08-04 20:08:01 -0700

    Time: 0 ms (100%), Space: 10.4 MB (88.99%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void fun(vector<int>& nums,int idx,int n,vector<int>&diary,vector<vector<int>>&res,vector<int>&used){
+        if(idx == n){
+            res.push_back(diary);
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(used[i]) continue;
+
+            used[i] =1;
+            diary.push_back(nums[i]);
+            fun(nums,idx+1,n,diary,res,used);
+            diary.pop_back();
+            used[i] =0;
+        }
+        
+        return;
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        int idx = 0;
+        vector<int>diary;
+        vector<vector<int>>res;
+        vector<int>used(n,0);
+        fun(nums,idx,n,diary,res,used);
+        
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
