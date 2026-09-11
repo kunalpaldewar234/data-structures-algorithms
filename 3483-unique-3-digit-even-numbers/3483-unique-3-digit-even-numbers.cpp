ec15a3128dc214ce7f2119aff584f4500ce74a1c
kunal paldewar <kunalpaldewar06@gmail.com>
2026-09-11 21:49:05 +0530

    Time: 3 ms (84.06%), Space: 30.9 MB (66.84%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        set<int> st;
+
+        for (int i = 0; i < digits.size(); i++) {
+            for (int j = 0; j < digits.size(); j++) {
+                for (int k = 0; k < digits.size(); k++) {
+
+                    if (i == j || j == k || i == k)
+                        continue;
+
+                    if (digits[i] == 0)
+                        continue;
+
+                    if (digits[k] % 2 != 0)
+                        continue;
+
+                    int num = digits[i] * 100
+                            + digits[j] * 10
+                            + digits[k];
+
+                    st.insert(num);
+                }
+            }
+        }
+
+        return st.size();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
