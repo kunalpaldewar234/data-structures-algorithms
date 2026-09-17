11b7bb4835cad5de7f7a93d13950aa5228ce5823
kunal paldewar <kunalpaldewar06@gmail.com>
2026-09-17 19:40:01 +0530

    Time: 58 ms (47.67%), Space: 25.6 MB (37.2%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,61 +1,51 @@
 class Solution {
 public:
+    bool isvalid(int i,int j,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m){
+            return false;
+        }
+        return true;
+    }
     int minimumEffortPath(vector<vector<int>>& heights) {
-
         int n = heights.size();
         int m = heights[0].size();
-
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
-
-        dist[0][0] = 0;
-        pq.push({0, {0, 0}});
-
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        while (!pq.empty()) {
-
-            auto top = pq.top();
+        vector<vector<int>>res(n);
+        for(int i=0;i<n;i++){
+            vector<int>t(m,INT_MAX);
+            res[i] = t;
+        }
+        priority_queue<pair<int,pair<int,int>>,
+                    vector<pair<int,pair<int,int>>>,
+                    greater<pair<int,pair<int,int>>>>pq;
+        int x[4] = {1,-1,0,0};
+        int y[4] = {0,0,1,-1};
+        res[0][0] = 0;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            pair<int,pair<int,int>>p = pq.top();
             pq.pop();
+            int dist = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
 
-            int currEffort = top.first;
-            int r = top.second.first;
-            int c = top.second.second;
-
-            if (r == n - 1 && c == m - 1)
-                return currEffort;
-
-            if (currEffort > dist[r][c])
+            if(dist > res[row][col]){
                 continue;
-
-            for (int i = 0; i < 4; i++) {
-
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-
-                    int diff = abs(heights[r][c] - heights[nr][nc]);
-
-                    int newEffort = max(currEffort, diff);
-
-                    if (newEffort < dist[nr][nc]) {
-
-                        dist[nr][nc] = newEffort;
-
-                        pq.push({newEffort, {nr, nc}});
-                    }
+            }
+            for(int k=0;k<4;k++){
+                int r = row+x[k];
+                int c = col+y[k];
+                if(!isvalid(r,c,n,m)){
+                    continue;
+                }
+                int absdiff = abs(heights[row][col]-heights[r][c]);
+                int newwt = max(dist,absdiff);
+                if(res[r][c] > newwt){
+                    res[r][c] = newwt;
+                    pq.push({newwt,{r,c}});
                 }
             }
         }
-
-        return 0;
+        return res[n-1][m-1];
     }
 };
 
